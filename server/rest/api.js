4ce8f04d09c9674d158d3538da0b9d08833a05fc
Luca <luca.colombo97@libero.it>
2021-07-19 14:00:53 +0200

    URL for heroku

--- server/rest/api.js
+++ server/rest/api.js
@@ -5,11 +5,17 @@ const app = express()
 // We need this one if we send data inside the body as JSON
 app.use(express.json())
 
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${PORT}`)
+})
+
 async function init() {
   // Call the init function that returns the Database
   const db = await initializeDatabase()
   // Let's extract all the objects we need to perform queries inside the endpoints
-  const { Person, Area, Product, AreaDetail, ProductDetail, Customer } = db._tables
+  const { Person, Area, Product, AreaDetail, ProductDetail, Customer } =
+    db._tables
   // API to get all the people
   app.get('/people', async (req, res) => {
     const people = await Person.findAll()
